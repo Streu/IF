c5fb0a152902565c64b73e79d0ce447648056c23
Streu <Streu1995@gmail.com>
2013-12-13 17:10:30 +0400

    1

--- IF/Program.cs
+++ IF/Program.cs
@@ -9,6 +9,19 @@ namespace IF
     {
         static void Main(string[] args)
         {
+            int i = Convert.ToInt32(Console.ReadLine());
+            int _i=i;
+            int k = (_i % 10)*1000;
+            _i =_i/ 10;
+            k = k+(_i % 10)*100;
+            _i = _i / 10;
+            k = k + (_i % 10) * 10;
+            _i = _i / 10;
+            k = k + (_i % 10);
+            if (i == k)
+                Console.WriteLine("Число читается одинаково слево направо и справа на лево");
+            Console.ReadKey();
+
         }
     }
 }
